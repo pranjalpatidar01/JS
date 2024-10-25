ae6e2acde8aa7b749ef908e5b661beb538c4c897
pranjalpatidar01 <155837935+pranjalpatidar01@users.noreply.github.com>
2024-10-25 05:08:08 +0000

    conversionsOperations

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,35 @@
+let score="33";
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+
+let age=22;
+let s=String(age);
+
+console.log(typeof(s));
+
+console.log(typeof(valueInNumber));
+
+
+//*******Operations ********
+
+let value=3;
+let negValue=-value;
+
+console.log(negValue);
+
+let s1="pranjal ";
+let s2="patidar";
+
+console.log(s1+s2);
+
+
+// when we add a string with any number then it will convert into string
+let one=100;
+
+console.log(one);
+one++;
+console.log(one);
+++one;
+console.log(one);
