9ef752e5dcea81e474227aeb2c1fa2dfc25da3c3
pranjalpatidar01 <155837935+pranjalpatidar01@users.noreply.github.com>
2024-10-25 04:54:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=1;
+let accountName="Pranjal";
+var accountPassword="1234"; //var is not used because of issue in block scope and function scope
+accountCity="Jaipur"  // don't prefer this way 
+
+let name; //undefined
+
+console.log(accountId);
+console.table([accountId,accountName,accountPassword]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello"); we are using node.js not browser
+
+// premetive datatypes
+let name="pranjal" //string
+let age=10 //number 2 to power 53
+let isFollow=true //boolean
+
+//null ans stand alone value - type object 
+// undefined means variable is declared but notassigned any value
+// symbol - unique
+
+// Non premetive datattype - object
