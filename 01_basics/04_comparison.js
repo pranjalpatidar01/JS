4dc8bdf55d58801f175dfcb1a0aaae683edc9755
pranjalpatidar01 <155837935+pranjalpatidar01@users.noreply.github.com>
2025-01-17 09:50:49 +0000

    Comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,3 +1,10 @@
 //> , < ,>= ,<= , == ,!=
+//  === strict check means datatypes need to be same
+// == not strict value need to be same datatype can be different 
 
 //while comparing make sure that datatypes should be same
+
+// Not a good practice
+// console.log("02" > 1);
+// console.log("2" > 1);  
+
