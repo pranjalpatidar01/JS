a2c591c3a9901b5aa6ba8359d2ad17f54b60ee50
pranjalpatidar01 <155837935+pranjalpatidar01@users.noreply.github.com>
2025-01-19 14:12:24 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,5 @@
-function sayMyname(){
+function sayMyname()
+{
     console.log("P");
     console.log('r');
     console.log('a');
@@ -7,13 +8,10 @@ function sayMyname(){
     console.log('a');
     console.log('l');
 }
-
 // sayMyname()
-
 function addTwoNumbers(number1, number2)
 {
     return (number1 + number2)
-    
 }
 
 const ans = addTwoNumbers(3, 5)
@@ -28,3 +26,32 @@ function userLogin(username = 'sam')
 let s = userLogin("Pranjal")
 
 console.log(s);
+
+
+function calculatCartValue(...num) {
+    return num
+}
+
+console.log(calculatCartValue(100, 200, 300));
+
+const user = {
+    name : "Pranjal",
+    age : 23 
+}
+
+function handleObject(anyObject)
+{
+    console.log(`Username is ${anyObject.name} and age is ${anyObject.age}`);
+    
+}
+
+handleObject(user)
+
+const arr = [200, 400, 500]
+
+function returnSecondValue(getArr)
+{
+    return getArr[1]
+}
+
+console.log(returnSecondValue(arr));
