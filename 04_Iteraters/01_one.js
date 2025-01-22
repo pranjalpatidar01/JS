d0e36fb34defaa8ac2d709f08ba7dd94cdea53a8
pranjalpatidar01 <155837935+pranjalpatidar01@users.noreply.github.com>
2025-01-22 09:36:58 +0000

    one

---
+++ 04_Iteraters/01_one.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i++) 
+{
+    const element = i;
+    // console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <=10; j++) {
+        const element = j;
+        // console.log(`${i} and ${j}`);
+        
+    }
+    
+}
